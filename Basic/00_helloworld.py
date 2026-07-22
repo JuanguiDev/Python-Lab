c206896b058c5f512811357fa26be2016b6f1883
JuanguiDev <jugulome28@gmail.com>
2026-07-21 20:20:58 -0500

    first commit

---
+++ Basic/00_helloworld.py
@@ -0,0 +1,17 @@
+# This is a sigle line comment.
+# This is other single line comment.
+
+'''
+  This is a multi-line comment.
+  Python is amazing programming language.
+'''
+
+# Data types.
+print(type(10))                 # Int
+print(type(3.14))               # Float
+print(type(1 + 3j))             # Complex number
+print(type('Juan'))             # String
+print(type([1, 2, 3]))          # List
+print(type({'name':'Juan'}))    # Dictionary
+print(type({9.8, 3.14, 2.7}))   # Set
+print(type((9.8, 3.14, 2.7)))   # Tuple
